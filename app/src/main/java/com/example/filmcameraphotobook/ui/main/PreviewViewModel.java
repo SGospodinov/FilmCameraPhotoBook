6b24a3a3b436a404e8458a1b084807f2bff94319
Stanislav Gospodinov <Stanislav.Gospodinov@bbc.co.uk>
2019-12-24 13:50:13 +0000

    Delete photos

--- app/src/main/java/com/example/filmcameraphotobook/ui/main/PreviewViewModel.java
+++ app/src/main/java/com/example/filmcameraphotobook/ui/main/PreviewViewModel.java
@@ -3,6 +3,8 @@ package com.example.filmcameraphotobook.ui.main;
 import androidx.lifecycle.ViewModel;
 
 import com.example.filmcameraphotobook.photo.Photo;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -10,17 +12,39 @@ public class PreviewViewModel extends ViewModel {
     private static final String userId = "qguMQjYyh1k8w8p8zES0";
 
     private final FirebaseStorage cloudStorage = FirebaseStorage.getInstance();
+    private final FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     private Photo photo;
+
+    private OnPhotoDeletedListener photoDeletedListener;
 
     public void setPhoto(Photo photo) {
         this.photo = photo;
+    }
+
+    public void setPhotoDeletedListener(OnPhotoDeletedListener photoDeletedListener) {
+        this.photoDeletedListener = photoDeletedListener;
     }
 
     public Photo getPhoto() {
         return photo;
     }
 
-    public StorageReference getPhotoRef() {
-        return cloudStorage.getReference().child(userId + "/" + photo.getPictureRef());
+    public StorageReference getPhotoImageRef() {
+        return cloudStorage.getReference().child(userId + "/" + photo.getPictureRef() + ".jpg");
+    }
+
+    public DocumentReference getPhotoRef() {
+        return firestore.document("users/" + userId + "/photos/" + photo.getId());
+    }
+
+    public void deletePhoto() {
+        getPhotoRef().delete().addOnCompleteListener(deletePhotoTask -> {
+            if (deletePhotoTask.isSuccessful()) getPhotoImageRef().delete()
+                    .addOnCompleteListener(deleteImageTask -> photoDeletedListener.onDeleted(photo));
+        });
+    }
+
+    public interface OnPhotoDeletedListener {
+        void onDeleted(Photo photo);
     }
 }
