e7f3cf2abd9fd86261a6c81b485b54bb401917cd
Stanislav Gospodinov <Stanislav.Gospodinov@bbc.co.uk>
2020-01-07 13:35:01 +0000

    Improve the landing page UI

--- app/src/main/java/com/example/filmcameraphotobook/ui/main/LandingFragment.java
+++ app/src/main/java/com/example/filmcameraphotobook/ui/main/LandingFragment.java
@@ -14,13 +14,18 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.filmcameraphotobook.R;
+import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Arrays;
 
+import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 public class LandingFragment extends Fragment {
@@ -28,11 +33,24 @@ public class LandingFragment extends Fragment {
     private LandingViewModel viewModel;
 
     private NavController navigationController;
+    private View loadingPanel;
+    private View errorPaner;
+    private TextView errorMessageTextView;
+    private TextView retryTextView;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.landing_fragment, container, false);
+        View view = inflater.inflate(R.layout.landing_fragment, container, false);
+
+        loadingPanel = view.findViewById(R.id.loading_panel);
+        errorPaner = view.findViewById(R.id.error_panel);
+
+        errorMessageTextView = view.findViewById(R.id.landing_error_message);
+        retryTextView = view.findViewById(R.id.retry_text_view);
+
+        retryTextView.setOnClickListener(textView -> startSignInActivity());
+        return view;
     }
 
     @Override
@@ -46,7 +64,9 @@ public class LandingFragment extends Fragment {
     }
 
     private void startSignInActivity() {
-        Intent signInActivityIntent = AuthUI.getInstance().createSignInIntentBuilder()
+        Intent signInActivityIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.AppTheme_NoActionBar)
                 .setAvailableProviders(Arrays.asList(
                         new AuthUI.IdpConfig.EmailBuilder().build(),
                         new AuthUI.IdpConfig.GoogleBuilder().build()
@@ -59,8 +79,24 @@ public class LandingFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == SIGN_IN_REQUEST_CODE && resultCode == RESULT_OK) {
+            displayLoadingPanel();
             navigateToGalleryFragment();
+        } else {
+            displayErrorMessage(IdpResponse.fromResultIntent(data));
         }
+    }
+
+    private void displayLoadingPanel() {
+        loadingPanel.setVisibility(View.VISIBLE);
+        errorPaner.setVisibility(View.GONE);
+    }
+
+    private void displayErrorMessage(IdpResponse response) {
+        if(response == null || response.getError() == null) errorMessageTextView.setText(R.string.unknown_error);
+        else if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) errorMessageTextView.setText(R.string.no_network);
+
+        loadingPanel.setVisibility(View.GONE);
+        errorPaner.setVisibility(View.VISIBLE);
     }
 
     private void navigateToGalleryFragment() {
